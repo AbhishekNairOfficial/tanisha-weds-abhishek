ac6ce06d40b87ef0ee952e29a8bf35e2ecda7974
Abhishek P <abhisheknr4@gmail.com>
2024-10-21 19:16:27 -0700

    Events CSS

--- src/organisms/Events/Events.tsx
+++ src/organisms/Events/Events.tsx
@@ -3,10 +3,12 @@ import EventCard from "../../molecules/EventCard/EventCard"
 
 const Events = () => {
     return (
-        <section className="flex flex-col align-center justify-center ">
-            <h1 className="text-primary font-primary text-3xl md:text-5xl underline m-auto">Reception &#38; Ceremony</h1>
+        <section className="flex flex-col align-center justify-center">
+            <h1 className="text-primary font-primary text-3xl md:text-5xl underline m-auto">
+                Reception &#38; Ceremony
+            </h1>
             <div className="h-16 md:h-24"></div>
-            <div className="flex flex-col md:flex mx-5 md:mx-[160px]">
+            <div className="flex flex-col justify-center md:flex-row mx-5 md:mx-[160px]">
                 {eventsConfig.eventsList.map((event) => (
                     <EventCard
                         key={event.date}
