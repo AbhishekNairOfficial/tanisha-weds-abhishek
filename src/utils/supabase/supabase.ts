23fbcef06cf7ec055e14f209c069aab2e1b1df21
Abhishek P <abhisheknr4@gmail.com>
2024-11-01 09:04:48 -0700

    Reading table name dynamically

--- src/utils/supabase/supabase.ts
+++ src/utils/supabase/supabase.ts
@@ -6,13 +6,15 @@ const supabase = createClient(
   import.meta.env.VITE_SUPABASE_PUBLIC_KEY
 );
 
+const tableName = import.meta.env.VITE_TABLE_NAME;
+
 export const saveRSVP = async (data: SaveRSVPInputProps): Promise<boolean> => {
   const payload = {
     ...data,
     created_at: new Date().toISOString().replace("T", " ").replace("Z", ""),
   };
   try {
-    const response = await supabase.from("RSVP").insert([payload]);
+    const response = await supabase.from(tableName).insert([payload]);
     if (response.error) {
       throw new Error("Error while saving");
     }
