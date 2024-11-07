66837c261ae0fbbbf61ddac4ccc2705eed9b23a4
Abhishek P <abhisheknr4@gmail.com>
2024-11-07 15:21:25 -0800

    Added speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import { Analytics } from "@vercel/analytics/react"
 import App from './App.tsx'
 import './index.css'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <Analytics />
+    <SpeedInsights />
   </StrictMode>,
 )
