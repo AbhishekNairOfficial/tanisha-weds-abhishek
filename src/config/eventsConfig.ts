967847b6d2354614a74b8981a8d034efc4a11812
Abhishek P <abhisheknr4@gmail.com>
2024-11-08 15:32:13 -0800

    reception time

--- src/config/eventsConfig.ts
+++ src/config/eventsConfig.ts
@@ -2,20 +2,20 @@ import RECEPTION_IMAGE from "../assets/reception-image.webp";
 import CEREMONY_IMAGE from "../assets/ceremony-image.webp";
 
 const eventsConfig = {
-    eventsList: [
-        {
-            title: "Reception",
-            image: RECEPTION_IMAGE,
-            date: "2025-01-19T19:00:00",
-            body: "A gathering of friends and family, dancing and dinner.",
-        },
-        {
-            title: "Muhurtham",
-            image: CEREMONY_IMAGE,
-            date: "2025-01-20T09:00:00",
-            body: "Saptapadi, Thaali kattu, traditional breakfast and lunch.",
-        },
-    ],
+  eventsList: [
+    {
+      title: "Reception",
+      image: RECEPTION_IMAGE,
+      date: "2025-01-19T18:30:00",
+      body: "A gathering of friends and family, dancing and dinner.",
+    },
+    {
+      title: "Muhurtham",
+      image: CEREMONY_IMAGE,
+      date: "2025-01-20T09:00:00",
+      body: "Saptapadi, Thaali kattu, traditional breakfast and lunch.",
+    },
+  ],
 };
 
 export default eventsConfig;
