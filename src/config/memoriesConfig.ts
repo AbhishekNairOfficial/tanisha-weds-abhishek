31ef86fcea9adb82dd5edae7179a7a2a47a8e4e7
Abhishek P <abhisheknr4@gmail.com>
2024-11-09 12:39:16 -0800

    Moved images to bucket

--- src/config/memoriesConfig.ts
+++ src/config/memoriesConfig.ts
@@ -1,59 +1,41 @@
-import MEMORIES_BANNER_1x from "../assets/memories-banner-image@1x.webp";
-import MEMORIES_BANNER_2x from "../assets/memories-banner-image@2x.webp";
-import MEMORIES_BANNER_3x from "../assets/memories-banner-image@3x.webp";
-
-import IMAGE_ONE_1X from "../assets/our-adventure/our-adventure-1@1x.webp";
-import IMAGE_ONE_2X from "../assets/our-adventure/our-adventure-1@2x.webp";
-import IMAGE_ONE_3X from "../assets/our-adventure/our-adventure-1@3x.webp";
-
-import IMAGE_TWO_1X from "../assets/our-adventure/our-adventure-2@1x.webp";
-import IMAGE_TWO_2X from "../assets/our-adventure/our-adventure-2@2x.webp";
-import IMAGE_TWO_3X from "../assets/our-adventure/our-adventure-2@3x.webp";
-
-import IMAGE_THREE_1X from "../assets/our-adventure/our-adventure-3@1x.webp";
-import IMAGE_THREE_2X from "../assets/our-adventure/our-adventure-3@2x.webp";
-import IMAGE_THREE_3X from "../assets/our-adventure/our-adventure-3@3x.webp";
-
-import IMAGE_FOUR_1X from "../assets/our-adventure/our-adventure-4@1x.webp";
-import IMAGE_FOUR_2X from "../assets/our-adventure/our-adventure-4@2x.webp";
-import IMAGE_FOUR_3X from "../assets/our-adventure/our-adventure-4@3x.webp";
+const BUCKET_URL = import.meta.env.VITE_BUCKET_URL;
 
 export const memoriesLandingConfig = {
-  "1x": MEMORIES_BANNER_1x,
-  "2x": MEMORIES_BANNER_2x,
-  "3x": MEMORIES_BANNER_3x,
+  "1x": `${BUCKET_URL}memories-banner/memories-banner-image@1x.webp`,
+  "2x": `${BUCKET_URL}memories-banner/memories-banner-image@2x.webp`,
+  "3x": `${BUCKET_URL}memories-banner/memories-banner-image@3x.webp`,
 };
 
 export const ourAdventureConfig = [
   {
     srcSet: {
-      "1x": IMAGE_ONE_1X,
-      "2x": IMAGE_ONE_2X,
-      "3x": IMAGE_ONE_3X,
+      "1x": `${BUCKET_URL}our-adventures/our-adventure-1@1x.webp`,
+      "2x": `${BUCKET_URL}our-adventures/our-adventure-1@2x.webp`,
+      "3x": `${BUCKET_URL}our-adventures/our-adventure-1@3x.webp`,
     },
     rotate: -6,
   },
   {
     srcSet: {
-      "1x": IMAGE_TWO_1X,
-      "2x": IMAGE_TWO_2X,
-      "3x": IMAGE_TWO_3X,
+      "1x": `${BUCKET_URL}our-adventures/our-adventure-2@1x.webp`,
+      "2x": `${BUCKET_URL}our-adventures/our-adventure-2@2x.webp`,
+      "3x": `${BUCKET_URL}our-adventures/our-adventure-2@3x.webp`,
     },
     rotate: -3,
   },
   {
     srcSet: {
-      "1x": IMAGE_THREE_1X,
-      "2x": IMAGE_THREE_2X,
-      "3x": IMAGE_THREE_3X,
+      "1x": `${BUCKET_URL}our-adventures/our-adventure-3@1x.webp`,
+      "2x": `${BUCKET_URL}our-adventures/our-adventure-3@2x.webp`,
+      "3x": `${BUCKET_URL}our-adventures/our-adventure-3@3x.webp`,
     },
     rotate: 6,
   },
   {
     srcSet: {
-      "1x": IMAGE_FOUR_1X,
-      "2x": IMAGE_FOUR_2X,
-      "3x": IMAGE_FOUR_3X,
+      "1x": `${BUCKET_URL}our-adventures/our-adventure-4@1x.webp`,
+      "2x": `${BUCKET_URL}our-adventures/our-adventure-4@2x.webp`,
+      "3x": `${BUCKET_URL}our-adventures/our-adventure-4@3x.webp`,
     },
     rotate: 12,
   },
